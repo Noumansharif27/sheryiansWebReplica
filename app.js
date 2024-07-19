635dea7aabc01fd2f1db874f382751f12301247b
Nouman Sharif <noumansharifgul27@gmail.com>
2024-07-19 14:20:51 +0500

    working on light mode

---
+++ app.js
@@ -0,0 +1,8 @@
+let navSignInBtn = document.querySelector("#nav #sign-in");
+let body = document.querySelector("#body");
+
+let isSignBtnClicked = 0;
+
+// navSignInBtn.addEventListener("click", () => {
+//   ReportBody.classList.toggle("light");
+// });
